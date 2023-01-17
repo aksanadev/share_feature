5597bbce7b90ec433e2776a49e1d90cbae676a01
Aksana Buster <aksana.buster@lexisnexis.com>
2023-01-17 11:51:41 -0600

    share icon button works without wrapper

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
+import 'dart:developer';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,13 +11,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GlobalKey _shareKey = GlobalKey();
+
+  _getPositions() {
+    final RenderBox shareBox =
+        _shareKey.currentContext!.findRenderObject() as RenderBox;
+    final positionShare = shareBox.localToGlobal(Offset.zero);
+    log('position: $positionShare');
+  }
+
+  _afterLayout(_) {
+    _getPositions();
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance!.addPostFrameCallback(_afterLayout);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page'), actions: const [
+      appBar: AppBar(title: const Text('Home Page'), actions: [
         Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Icon(CupertinoIcons.share)),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: IconButton(
+              key: _shareKey,
+              icon: const Icon(CupertinoIcons.share),
+              onPressed: () {
+                _getPositions();
+                final RenderBox shareBox = _shareKey.currentContext!.findRenderObject() as RenderBox;
+                final positionShare = shareBox.localToGlobal(Offset.zero);
+                Share.share('Testing', subject: '', sharePositionOrigin:Rect.fromLTWH(positionShare!.dx, positionShare.dy, shareBox!.size.width, shareBox.size.height));
+              },
+            )),
       ]),
     );
   }
